16743b1aa38f37a608e97a8dbcbb460d49d7bc4a
Ronak Singh <ronaksingh93575@gmail.com>
2026-06-19 16:21:13 +0530

    code

---
+++ logs/logger.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+class Logger:
+
+    callback = None
+
+    @classmethod
+    def set_callback(cls, callback):
+        cls.callback = callback
+
+    @classmethod
+    def log(cls, message):
+
+        timestamp = datetime.now().strftime(
+            "%Y-%m-%d %H:%M:%S"
+        )
+
+        final_message = (
+            f"[{timestamp}] {message}"
+        )
+
+        print(final_message)
+
+        if cls.callback:
+            cls.callback(final_message)
+
+        with open("logs/activity.log",
+                   "a",
+                   encoding="utf-8"
+                ) as log_file:
+            log_file.write(final_message + "\n")
