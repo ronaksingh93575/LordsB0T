c867a9006babf86fc9cb92cc8b5caf3ae6b26c9e
Ronak Singh <ronaksingh93575@gmail.com>
2026-06-24 13:52:03 +0530

    (￣^￣ )ゞ

--- tasks/train_troops.py
+++ tasks/train_troops.py
@@ -13,11 +13,31 @@ import time
 import pyautogui
 from logs.logger import Logger
 from ui import settings
+from vision.image_finder import find_image
 
 
 def run(username):
     #pause for 2 sec
     Logger.log("confirming Home Screen")
-    time.sleep(1)
-
+    time.sleep(1)   
     
+    #----------------------------------
+    # cehcking if we are at home or not
+    #----------------------------------
+    location = find_image("images/kingdom_map_1")
+    Logger.log("Attempt 1")
+
+    if not location:
+        location = find_image("images/kingdom_map_2")
+
+    elif not location:
+        location = find_image("images/kingdom_map_3")
+
+    elif not location:
+        location = find_image("images/kingdom_map_4")
+
+    if location:
+        Logger.log(f"Home Kingdom Verified :{location}")
+
+    else:
+        pass
